e10f8ebf063a020cfdf8b16bf49a060c59d4a450
zy-李炯炯 <17634397278@163.com>
2023-06-23 16:47:42 +0800

    格式化

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,21 @@
 </template>
 
 <script setup lang="ts">
-const str = '爱说大组该哦'
+const str = '爱说大组该哦';
+function f1() {
+
+
+  console.log('xxx');
+
+
+
+}
+
+
+
+
+
+
 </script>
 
 <style scoped lang="scss">
