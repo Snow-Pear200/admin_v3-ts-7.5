a57f55cad992dbd2ff5c6389cdec59ca89e9a9d1
zy-李炯炯 <17634397278@163.com>
2023-06-23 16:37:42 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <h1>我是App根组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '爱说大组该哦'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: #000;
+}
+</style>
